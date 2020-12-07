c01b0d41af7426098440ee54e9e4dcfbfc6b93b5
Ilja Patrushev <slowtnr@gmail.com>
2020-12-07 16:06:14 +0200

    Initial Commit

---
+++ JSONDApiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSONDApiApp
+//
+//  Created by Ilja Patrushev on 7.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
